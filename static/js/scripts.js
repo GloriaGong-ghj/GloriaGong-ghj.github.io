51b0d71845b1dec3ea5ce5b0b6be83291247e1b1
GloriaGong-ghj <2692416776@qq.com>
2025-04-19 17:49:37 +0800

    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'experience','certifications'];
+const section_names = ['home','experience','certifications','profile'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
